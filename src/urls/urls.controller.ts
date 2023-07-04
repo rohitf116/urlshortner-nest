172b0d69c7df9e3f946d1a898b41c275cb020025
rohitf116 <rrsonawne090909@gmail.com>
2023-07-04 14:28:26 -0700

    code added

--- src/urls/urls.controller.ts
+++ src/urls/urls.controller.ts
@@ -8,40 +8,40 @@ import {
   Delete,
   UseInterceptors,
   Req,
+  Res,
 } from '@nestjs/common';
 import { UrlsService } from './urls.service';
 import { CreateUrlDto } from './dto/create-url.dto';
 import { UpdateUrlDto } from './dto/update-url.dto';
 import { MainInterceptor } from 'src/interpreter/main.interseptor';
-
-@Controller('urls')
+import { Response } from 'express';
+@Controller('')
 export class UrlsController {
   constructor(private readonly urlsService: UrlsService) {}
 
   @Post()
   @UseInterceptors(MainInterceptor)
-  create(@Body() createUrlDto: CreateUrlDto, @Req() req: any) {
-    console.log(req.url, 'Req');
-    return this.urlsService.create(createUrlDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.urlsService.findAll();
+  create(
+    @Body() createUrlDto: CreateUrlDto,
+    @Req() req: any,
+    @Res() res: Response,
+  ) {
+    return this.urlsService.create(createUrlDto, req.url, res);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.urlsService.findOne(+id);
+  findOne(@Param('id') id: string, @Res() res: Response) {
+    console.log(id);
+    return this.urlsService.findOne(id, res);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUrlDto: UpdateUrlDto) {
-    return this.urlsService.update(+id, updateUrlDto);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateUrlDto: UpdateUrlDto) {
+  //   return this.urlsService.update(+id, updateUrlDto);
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.urlsService.remove(+id);
-  }
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.urlsService.remove(+id);
+  // }
 }
--- src/urls/urls.service.ts
+++ src/urls/urls.service.ts
@@ -1,26 +1,60 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  HttpStatus,
+  HttpException,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateUrlDto } from './dto/create-url.dto';
 import { Model, Connection, Types } from 'mongoose';
 import { InjectModel, InjectConnection } from '@nestjs/mongoose';
 import { UpdateUrlDto } from './dto/update-url.dto';
 import { Url } from './model/url.model';
 import { RandomStringService } from './utils/randomString.util';
-
+import { Response } from 'express';
 @Injectable()
 export class UrlsService {
   constructor(
-    @InjectModel(Url.name) private userModel: Model<Url>,
+    @InjectModel(Url.name) private urlModel: Model<Url>,
     @InjectConnection() private connection: Connection,
     private randomStringService: RandomStringService,
   ) {}
-  async create(createUrlDto: CreateUrlDto) {}
+  async create(createUrlDto: CreateUrlDto, url: string, res: Response) {
+    const foundUrl = await this.urlModel.findOne({
+      longUrl: createUrlDto.longUrl,
+    });
+    if (foundUrl) {
+      return res.status(HttpStatus.OK).json(foundUrl);
+    }
+    const urlCode = this.randomStringService.generateRandomString(6);
+    const shortUrl = `${url}/${urlCode}`;
+    const newUrl = await this.urlModel.create({
+      ...createUrlDto,
+      urlCode,
+      shortUrl,
+    });
+    if (!newUrl) {
+      throw new HttpException(
+        'Failed to create URL',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+
+    return {
+      statusCode: HttpStatus.CREATED,
+      data: newUrl,
+    };
+  }
 
   findAll() {
     return `This action returns all urls`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} url`;
+  async findOne(id: string, res: Response) {
+    const url = await this.urlModel.findOne({ urlCode: id });
+    if (!url) {
+      throw new NotFoundException('Url not found');
+    }
+    res.redirect(url.longUrl);
   }
 
   update(id: number, updateUrlDto: UpdateUrlDto) {
