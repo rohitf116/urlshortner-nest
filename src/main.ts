dfe741501389230fac4fa7e8ec8103347858a00d
rohitf116 <rrsonawne090909@gmail.com>
2023-07-06 10:30:37 -0700

    bug fixed

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cors from 'cors';
+import { config } from 'dotenv';
+config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Enable CORS
   app.use(cors());
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
